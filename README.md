1d33de50ec49719cde047cddfafc54e282ed6d90
Steven Perez <techguy530@gmail.com>
2020-04-11 22:53:33 -0400

    update login

---
+++ README.md
@@ -0,0 +1,33 @@
+![NodeJS Master](https://github.com/stevenpl98/FoodUniversity/workflows/NodeJS%20Master/badge.svg)
+# Food University
+
+## Introduction
+
+
+## Features
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9.
+
+
+## Getting Started
+
+### Installation and Setup
+
+### Run
+
+## Demo video
+
+## Contributors
+
+* Steven Perez, Team Lead
+*
+*
+*
+
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('FoodUniversity', () => App);
+AppRegistry.registerComponent('FoodUni', () => App);
 
 if (Platform.OS === 'web') {
   const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('FoodUniversity', { rootTag });
+  AppRegistry.runApplication('FoodUni', { rootTag });
 }
